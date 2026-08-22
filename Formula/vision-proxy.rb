47c66430aca4a15b466ab84cb3dbe6900045c01a
tan-yong-sheng <64836390+tan-yong-sheng@users.noreply.github.com>
2026-08-22 13:58:31 +0800

    release: v0.1.3

--- Formula/vision-proxy.rb
+++ Formula/vision-proxy.rb
@@ -11,7 +11,7 @@
 class VisionProxy < Formula
   desc "CLI that routes images to a vision model for agent UserPromptSubmit hooks"
   homepage "https://github.com/tan-yong-sheng/vision-proxy"
-  version "0.1.0"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
